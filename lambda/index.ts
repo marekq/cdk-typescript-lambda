50c4b5e9a75f712eee47c1a8b27e75beadb098d8
Marek Kuczynski <marek.kuczynski@gmail.com>
2021-07-12 23:40:02 +0200

    Added api_path, added readme

--- lambda/index.ts
+++ lambda/index.ts
@@ -11,16 +11,20 @@ const ddb_client = new DynamoDB.DocumentClient({ region: AWS_REGION });
 // Lambda handler
 export async function Handler(event: APIGatewayProxyEventV2): Promise<APIGatewayProxyResultV2> {
     
+    console.log(event);
+
     // Get timestamp and client IP
     const timest = Math.floor(Date.now() / 1000);
     const client_ip = event.requestContext.http.sourceIp;
+    const api_path = event.rawPath;
 
     // Build DynamoDB parameters
     const params = {
         TableName: TABLE_NAME,
         Item: {
             timest: timest.toString(),
-            client_ip: client_ip
+            client_ip: client_ip,
+            api_path: api_path
         }
     };
 
@@ -32,6 +36,6 @@ export async function Handler(event: APIGatewayProxyEventV2): Promise<APIGateway
     // Return 200 OK to client
     return {
         statusCode: 200,
-        body: JSON.stringify({ message: 'hello lambda ' + timest + ' ' + client_ip })
+        body: JSON.stringify({ message: 'hello lambda ' + timest + ' ' + client_ip + ' ' + api_path})
     };
 };
---
+++ readme.md
@@ -0,0 +1,4 @@
+cdk-typescript-lambda
+---------------------
+
+My CDK project to deploy an API Gateway, Lambda and DynamoDB stack using Typescript.
